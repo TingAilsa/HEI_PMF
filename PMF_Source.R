d3fb41862e187fa1319d2c0cbff68bce72ca1c6f
Ting Zhang <TingZhang@L-MAC-N04097.eduroam.gmu.edu>
2023-02-20 22:01:22 -0500

    change the work directory

--- PMF_Source.R
+++ PMF_Source.R
@@ -1,7 +1,64 @@
 ##set working directory
-setwd("/Users/ztttttt/Documents/HEI PMF/CSN_IMPROVE/PMF_output")
+setwd("/Users/TingZhang/Documents/HEI HAQ PMF/PMF_try/for R")
 getwd()
-data.dir <- "/Users/ztttttt/Documents/HEI PMF/CSN_IMPROVE/PMF_Results"
+data.dir <- "/Users/TingZhang/Documents/HEI HAQ PMF/PMF_try/for R"
+
+#### CSN EPA-MDL, PMF output linear - clusters ####
+csn_pmf_base = read.csv("EPA_MDL_CSN_Cluster6_F7.csv")
+colnames(csn_pmf_base)[1] = "Date"
+factor.number = ncol(csn_pmf_base) - 1
+cluster.study = "Cluster6"
+# cluster.study = "Cluster24"
+
+# there is formatting wrong in date due to influence of excel, so use the date from concentration file
+csn_conc = read.csv("/Users/ztttttt/Dropbox/HEI_PMF_files_Ting/CSN_PMF_Try_data/EPA MDL/CSN_Cluster6_2011-14_PMF_conc_EPAmdl.csv")
+csn_pmf_base$Date = csn_conc$Date
+csn_pmf_base$Date = as.Date(csn_pmf_base$Date, format = "%m/%d/%y")
+
+# match the total PM2.5 mass concentration
+csn_pmf_base$P25 = csn_conc$PM25
+
+# match the sidecode of sites in given cluster
+site_row = read.csv("/Users/ztttttt/Dropbox/HEI_PMF_files_Ting/CSN_PMF_Try_data/EPA MDL/CSN_Cluster6_date_number_of_site_EPAmdl.csv")
+csn_pmf_base$SiteCode = rep(unique(site_row$SiteCode), site_row$Freq)
+
+csn_pmf_base = csn_pmf_base[!duplicated(csn_pmf_base), ]
+csn_pmf_base <- csn_pmf_base[-2, ]
+
+###### CSN EPA-MDL C6F7, lm, all sites ######
+# use linear regression to get the factor contribution
+csn_pmf_base_forLM = csn_pmf_base
+csn_pmf_base_forLM$Date = csn_pmf_base_forLM$SiteCode = NULL
+csn_pmf_base_lm = lm(P25 ~., data = csn_pmf_base_forLM)
+csn_pmf_base_lm_beta = summary(csn_pmf_base_lm)$coefficients[, 1]
+csn_pmf_base_lm_beta = data.frame(csn_pmf_base_lm_beta[2:length(csn_pmf_base_lm_beta)])
+colnames(csn_pmf_base_lm_beta) = "lm.factor.beta.cluster"
+csn_pmf_base_lm_beta$Factor = rownames(csn_pmf_base_lm_beta)
+csn_pmf_base_lm_beta$Factor.contribution = (csn_pmf_base_lm_beta$lm.factor.beta.cluster/
+                                              sum(csn_pmf_base_lm_beta$lm.factor.beta.cluster))*100
+
+write.csv(csn_pmf_base_lm_beta, 
+          paste0("CSN_", cluster.study, "F", factor.number, "2011-14_EPAmdl_PMF_overall.csv"))
+
+###### CSN EPA-MDL C6F7, lm, each sites ######
+csn_pmf_base_site3 = subset(csn_pmf_base,
+                            SiteCode == site_row$SiteCode[3])
+csn_pmf_base_site3_lm = lm(P25 ~ Factor.1 + Factor.2 + Factor.3 + Factor.4 + 
+                             Factor.5 + Factor.6 + Factor.7, 
+                           data = csn_pmf_base_site3)
+csn_pmf_base_site3_lm_beta = summary(csn_pmf_base_site3_lm)$coefficients[, 1]
+csn_pmf_base_site3_lm_beta = data.frame(csn_pmf_base_site3_lm_beta[2:length(csn_pmf_base_site3_lm_beta)])
+colnames(csn_pmf_base_site3_lm_beta) = "lm.beta.site-ClusterBased"
+csn_pmf_base_site3_lm_beta$Factor = rownames(csn_pmf_base_site3_lm_beta)
+csn_pmf_base_site3_lm_beta$Factor.contribution = (csn_pmf_base_site3_lm_beta$lm.beta.site/
+                                                    sum(csn_pmf_base_site3_lm_beta$lm.beta.site))*100
+
+write.csv(csn_pmf_base_site3_lm_beta, "CSN_Cluster6_2011-14_EPAmdl_PMF_F7_site3.csv")
+
+
+
+
+
 
 #### CSN EPA-MDL, Cluster6 & 7 factors ####
 csn_eap_C6f7 = read.csv("EPA_MDL_CSN_Cluster6_F7.csv")
