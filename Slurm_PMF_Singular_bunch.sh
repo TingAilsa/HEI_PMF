11ed2498e9f5a1a24baa0102f818407e70ff5509
Ting Zhang <TingZhang@L-MAC-N04097.eduroam.gmu.edu>
2023-08-31 13:21:12 -0400

    commit test

---
+++ Slurm_PMF_Singular_bunch.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+#SBATCH --partition=normal
+#SBATCH --job-name=wine_run
+
+## Specify the needed settings from the server 
+#SBATCH --nodes=1  # number of nodes
+#SBATCH --tasks-per-node=1  # tasks per node # up to 128;
+#SBATCH --mem-per-cpu=10G  # amount of memory the job requires, default is 2G  # memory per CORE
+
+## Assign the name of job, output & error files
+#SBATCH --job-name=pmf_noGUI_try
+## NOTE: %u=userID, %x=jobName, %N=nodeID, %j=jobID, %A=arrayID, %a=arrayTaskID
+#SBATCH --output=pmf_noGUI_try_%N_%j.out # output file
+#SBATCH --error=pmf_noGUI_try_%N_%j.err # error file
+
+## Email info for updates from Slurm
+#SBATCH --mail-type=BEGIN,END,FAIL # ALL,NONE,BEGIN,END,FAIL,REQUEUE,..
+#SBATCH --mail-user=tzhang23@gmu.edu
+
+## Specify the maximum of time in need
+#SBATCH --time=03-00:00  # Total time needed for job: Days-Hours:Minutes
+
+## create an array of jobs
+#SBATCH --array=1-150
+
+#load modules
+module load r 
+module load singularity
+
+SINGULARITY_BASE=/containers/hopper/Containers
+CONTAINER=${SINGULARITY_BASE}/wine/wine.sif
+SINGULARITY_RUN="singularity exec  -B ${PWD}:/host_pwd --pwd /host_pwd"
+SCRIPT=PMF_bs_6f8xx_sealed_GUI_MOD.ini
+
+# ${SINGULARITY_RUN} ${CONTAINER} wine ${PWD}/ME-2.exe ${SCRIPT}  
+
+# Define the DOS command to be used
+DOS_COMMAND="${SINGULARITY_RUN} ${CONTAINER} wine ${PWD}/ME-2.exe ${SCRIPT}" 
+
+# Calculate the Cluster and Factor numbers based on the array index
+Cluster_number=$(( ( ($SLURM_ARRAY_TASK_ID - 1) / 6 ) + 1 ))
+Factor_number=$(( ( ($SLURM_ARRAY_TASK_ID - 1) % 6 ) + 6 ))
+
+# Set the directory path for the Cluster and Factor folders
+cd "Cluster_${Cluster_number}/Factor_${Factor_number}"
+
+## Run the tasks
+
+# 1. Run DOS command for PMF base model analysis
+cp iniparams_base.txt iniparams.txt
+$DOS_COMMAND
+rm iniparams.txt 
+
+# 2. Analyze the output .txt file, generate the new value for numoldsol, and replace it in other iniparams.txt series using R
+mv CSN_C_${Cluster_number}_F_${Cluster_number}_.txt CSN_C_${Cluster_number}_F_${Factor_number}_base.txt
+Rscript /projects/HAQ_LAB/tzhang/pmf_no_gui/file_try/minQ_Task_numoldsol.R CSN_C_${Cluster_number}_F_${Factor_number}_base.txt
+
+# 3. Run DOS command for BS, DISP, and BS-DISP analyses in turn 
+for param_file in iniparams_BS.txt iniparams_DISP.txt # iniparams_BS_DISP.txt
+do
+  cp ${param_file} iniparams.txt
+  $DOS_COMMAND 
+  rm iniparams.txt
+done
+# rename the output from DISP
+mv DISPres1.txt CSN_C_${Cluster_number}_F_${Factor_number}_DISPres1.txt
+
+
+
+
